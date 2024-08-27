96e9866b88ba14d0ab21ee0b31f270774b2da0fc
Winkie <ay1219723679@gmail.com>
2024-08-27 11:12:47 -0400

    CRUD fucntions of todolist

--- ToDo/views.py
+++ ToDo/views.py
@@ -3,8 +3,12 @@ from todolist.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    print (completed_tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
+
     }
     print (tasks)
     return render(request,'home.html', context)
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -8,3 +9,34 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home') 
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home') 
+
+def edit(request, pk):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=pk)
+        context = {
+            'task': task,
+        }
+        return render(request,'edit.html', context)
+    
+    if request.method == 'POST':
+        task = get_object_or_404(Task, pk=pk)
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+
+def delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home') 
